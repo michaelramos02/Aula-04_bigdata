feb329039622155817d6acd47f64e3030cf74b35
michaelramos02 <michaelramos1502@gmail.com>
2026-04-07 19:52:30 -0300

    iniciando repostório

---
+++ exemplo01.py
@@ -0,0 +1,24 @@
+# # Estrutura if
+
+# idade = int(input('Digite a sua idade: '))
+
+# if idade >= 18: 
+#      print('Maior de idade')
+
+# elif idade >= 15:
+#      print('Quase adulto')
+# else:
+#      print('Menor de idade')
+
+
+# Estrutura de pontuação
+     
+
+pontos = float(input('Digite os seus pontos: '))
+
+if pontos >= 100:
+     print('Execelente!')
+elif pontos >= 50:
+     print('Bom desemp')
+else:
+     print('Pode melhorar')
